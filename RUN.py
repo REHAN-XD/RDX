cd8e9a2f398f90ada42e7a70308513bcfcd529f7
Rehan <119917711+REHAN-XD@users.noreply.github.com>
2023-02-26 22:12:42 +0500

    Add files via upload

--- RUN.py
+++ RUN.py
@@ -1,3 +1,15 @@
-import os
-from RDX import rehan
-rehan()
+import os, platform
+
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from RDX64 import intro
+    intro()
+elif bit == '32bit':
+    print(f"     {purple}╓═════{purple}════{yellow}─────────────────{purple}════{purple}════════╖{white}")
+    print(f"     {yellow}│{white}【{green}✘{white}】{yellow} DEVICE DOESNT SUPPORT THIS TOOL {yellow}│")
+    print(f"     {purple}╙{purple}═════════{yellow}─────────────────{purple}════════════{purple}╜{white}")
